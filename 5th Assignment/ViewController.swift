12f8586cb559e7b51555b706e62a1b1728dc3f83
Bread-kn72 <ordinarytom72@gmail.com>
2024-05-07 11:43:49 +0900

    Initial Commit

---
+++ 5th Assignment/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  5th Assignment
+//
+//  Created by Kinam on 5/7/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
